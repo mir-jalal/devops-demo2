bee9299e71b612e6ce6ff41449129dc343538818
mir-jalal <mircelal99@gmail.com>
2021-03-12 05:32:11 +0000

    Add dockerfiles and other related files

---
+++ bootstrap_app.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+apt-get -y update
+
+cd /home/demo2 || exit
+
+git clone https://github.com/spring-projects/spring-petclinic.git
+
+cd ./spring-petclinic || exit
+
+chmod +x mvnw
+
+./mvnw clean package
+
+
